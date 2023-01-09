a3616e02f9c4555edf4cf11f9ea8204a3303b1db
Jeffrey <1115378579@qq.com>
2023-01-09 18:02:20 +0800

    Initial commit

---
+++ src/layouts/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="text-center text-gray-700 dark:text-gray-200">
+    <Header />
+    <RouterView />
+    <div class="mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm">
+      [Home Layout]
+    </div>
+  </main>
+</template>
